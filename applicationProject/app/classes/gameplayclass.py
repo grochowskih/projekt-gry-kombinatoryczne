4d79c3789fb6d37b3f06f8d801208b19090a854e
k2pel <64159134+k2pel@users.noreply.github.com>
2020-05-02 12:01:30 +0200

    Comment everything, because it's hard for me to make good obiektówka

--- applicationProject/app/classes/gameplayclass.py
+++ applicationProject/app/classes/gameplayclass.py
@@ -1,3 +1,16 @@
-class Gameplay:
+#from app.mathmodule.mathmodule import series_one_colour
+
+'''class Gameplay:
     #TODO: Implementacja graczy, sprawdzanie czy koniec gry, obsługa wyswietlania planszy
-    def __init__():
+    def __init__(self, n, k):
+        self.n = n
+        self.k = k
+        self.first_player = first_player
+        self.second_player = second_player
+
+    def create_a_dict(self):
+        dict = {}
+        for i in range(1, self.n + 1):
+            dict[i] = 0
+        return dict
+        '''
