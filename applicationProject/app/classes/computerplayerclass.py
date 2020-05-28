c6b5186bd016703d022319490c7972ec02cea6ff
grochowskih <grochowskihub@gmail.com>
2020-05-28 22:19:04 +0200

    Add blank lines

--- applicationProject/app/classes/computerplayerclass.py
+++ applicationProject/app/classes/computerplayerclass.py
@@ -28,20 +28,25 @@ class RandomComputerPlayer(ComputerPlayer):
     #TODO Implementacja wyboru par, kolorowania itp. w strategii losowej
     def choose_two(self):
         return random.sample(without_color(self.d, self.n), 2)
+
     def color_one(self, first_number, second_number):
         list = [first_number, second_number]
         return random.choice(list)
+
 
 class EgoisticComputerPlayer(ComputerPlayer):
     # TODO Implementacja wyboru par, kolorowania itp. w strategii egoistycznej
     def choose_two(self):
         return choose_2_numbers(self.n, self.k, self.d, EgoisticComputerPlayer.real_players_color(self), self.color)
+
     def color_one(self, first_number, second_number):
         return choose_from_2_numbers(self.n, self.k, self.d, self.color, first_number, second_number)
+
 
 class DuelingComputerPlayer(ComputerPlayer):
     # TODO Implementacja wyboru par, kolorowania itp. w strategii atakującej
     def choose_two(self):
         return choose_2_numbers(self.n, self.k, self.d, DuelingComputerPlayer.real_players_color(self), DuelingComputerPlayer.real_players_color(self))
+
     def color_one(self, first_number, second_number):
         return choose_from_2_numbers(self.n, self.k, self.d, DuelingComputerPlayer.real_players_color(self), first_number, second_number)
